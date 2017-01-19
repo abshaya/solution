ffa179b3556170fae5a7c2a38c0d6f70f5175ea3
abshaya <abshaya1996@gmail.com>
2017-01-19 22:17:13 +0530

    java pgm

---
+++ greaterr.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class greaterr
+{
+    public static void main(String[] args) {
+        Scanner sn=new Scanner(System.in);
+        char n;
+        n=sn.next().charAt(0);
+        if((65<=n&&n<=90)||(97<=n&&n<=122))
+            System.out.format("%d",(int)n);
+        else
+            System.out.println("not an alphabet");
+        
+}
+           
+}
