84e1c065e5531e6b9aec58dbf543b701ac5ad1d8
abshaya <abshaya1996@gmail.com>
2017-01-20 20:45:13 +0530

    java pgm

---
+++ helloprint.java
@@ -0,0 +1,17 @@
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class helloprint
+{
+    public static void main(String[] args) {
+        
+            int i=5;
+           while(i>0)
+           {
+               System.out.println("Hello");
+               i--;
+           }
+}
+           
+}
